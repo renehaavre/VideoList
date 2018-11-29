e7e456e1ff8d03dce6b16b8bcfd53d34dd1e405b
Rene Haavre <haavre@gmail.com>
2018-11-29 20:34:04 +0200

    Initial Commit

---
+++ VideoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoList
+//
+//  Created by Rene Haavre on 29/11/2018.
+//  Copyright © 2018 Rene Haavre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
